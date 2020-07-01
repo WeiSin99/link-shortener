8c8a8fd12c1df9338efff1d0ea0599f2eba3b08c
WeiSin99 <looweisin008@gmail.com>
2020-07-01 17:29:52 +0800

    Shorten a invalid link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,21 @@
 class LinksController < ApplicationController
 
   def new
+    @link = Link.new
   end
 
+  def create
+    @link = Link.new(link_params)
+    if @link.save
+
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+
+    def link_params
+      params.require(:link).permit(:original_link)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'links#new'
+  resources :links, only: :create
 end
